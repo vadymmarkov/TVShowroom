096a9258af4960ae07527139c470b09edea27780
Vadym Markov <markov.vadym@gmail.com>
2018-10-10 13:31:35 +0200

    Update collections demo

--- TVShowroom/Components/Collection.swift
+++ TVShowroom/Components/Collection.swift
@@ -150,15 +150,6 @@ private func generateRandomData() -> [[UIColor]] {
     let numberOfItemsPerRow = 15
 
     return (0..<numberOfRows).map { _ in
-        return (0..<numberOfItemsPerRow).map { _ in UIColor.randomColor() }
-    }
-}
-
-private extension UIColor {
-    class func randomColor() -> UIColor {
-        let hue = CGFloat(arc4random() % 100) / 100
-        let saturation = CGFloat(arc4random() % 100) / 100
-        let brightness = CGFloat(arc4random() % 100) / 100
-        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
+        return (0..<numberOfItemsPerRow).map { _ in UIColor.lightGray }
     }
 }
